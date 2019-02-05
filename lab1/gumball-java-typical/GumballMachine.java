dd57f878fe3b1df3cf4637d1e263d2961964e0e5
Wamique Ansari <wamiqueansari@Wamiques-MacBook-Pro.local>
2019-02-05 11:17:21 -0800

    Changed the implementation from 3 classes to single class for better Code Maintainability and Reusability

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,62 +3,85 @@ public class GumballMachine
 {
 
     protected int num_gumballs;
-    protected boolean has_required_amount;
+    private int accepted_coins[];
+    private int cost;
+    protected boolean has_allowed_coin;
+    private boolean has_required_amount;
     protected int total_received_amount;
     protected int remaining_amount;
     protected int required_amount;
-    protected int extra_cents;
+    protected int extra_amount;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int cost, int accepted_coins[] )
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_required_amount = false;
         this.total_received_amount = 0;
-        this.extra_cents = 0;
+        this.extra_amount = 0;
+        this.cost = cost;
+        this.remaining_amount = this.cost;
+        this.accepted_coins = accepted_coins;
+        this.has_required_amount = false;
+        this.has_allowed_coin = false;
     }
 
     public void insertCoin(int coin){
-        this.total_received_amount = this.total_received_amount + coin;
-        this.remaining_amount = this.remaining_amount - coin;
+        for(int c : accepted_coins)
+        {
+            if(c == coin)
+            {
+                this.has_allowed_coin = true;
+                break;
+            }
+            
+        }
+        
+        if(this.has_allowed_coin)
+        {
+                this.total_received_amount = this.total_received_amount + coin;
+                this.remaining_amount = this.remaining_amount - coin;
+                this.has_allowed_coin = false;
+        }
+        else
+        {
+                System.out.println("Accepts only quarter");
+        }
+        
+        if(this.total_received_amount >= this.cost)
+        {
+            this.has_required_amount = true;
+            this.extra_amount = this.total_received_amount - this.cost;
+        }
     }
 
     public void turnCrank()
     {
-        confirmRequiredAmount();
         if ( this.has_required_amount )
         {
             if ( this.num_gumballs > 0 )
             {
                 this.num_gumballs-- ;
+                this.total_received_amount = 0;
                 this.has_required_amount = false ;
-                if(this.extra_cents > 0)
+                if(this.extra_amount > 0)
                 {
-                    System.out.println( "Thanks for your "+this.required_amount+ "cents.  Gumball Ejected! Can't return your "+this.extra_cents +"cents." ) ;
+                    System.out.println( "Thanks for your "+this.cost+ "cents.  Gumball Ejected! Can't return your "+this.extra_amount +"cents." ) ;
                 } 
                 else
                 {
-                    System.out.println( "Thanks for your "+this.required_amount+ "cents.  Gumball Ejected!" ) ;
+                    System.out.println( "Thanks for your "+this.cost+ "cents.  Gumball Ejected!" ) ;
                 }
                 
             }
             else
             {
-                System.out.println( "No More Gumballs!  Sorry, can't return your "+(this.required_amount+this.extra_cents)+ " cents." ) ;
+                System.out.println( "No More Gumballs!  Sorry, can't return your "+(this.cost+this.extra_amount)+ " cents." ) ;
+                this.extra_amount = 0;
             }
         }
         else 
         {
             System.out.println( "Cannot eject the gumball. Please insert more "+this.remaining_amount +" cents" ) ;
         }        
-    }
-    
-    public void confirmRequiredAmount(){
-        if(this.total_received_amount >= this.required_amount){
-            this.has_required_amount = true;
-            this.extra_cents = this.total_received_amount - this.required_amount;
-            this.total_received_amount = 0;
-            this.remaining_amount = this.required_amount;
-        }
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -1,73 +1,46 @@
 public class Main {
     
-   final static int nickle = 5;
-   final static int dime = 10;
-   final static int quarter = 25;
-    
-   public static void main(String[] args) {
-       
-        Machine1 machine1 = new Machine1(4);
-        Machine2 machine2 = new Machine2(4);
+    public static void main(String[] args) {
+        GumballMachine gumball_machine1 = new GumballMachine(2,50,new int[]{5,10,25});
+
+        System.out.println(gumball_machine1);
+
+        gumball_machine1.insertCoin(5);
+        gumball_machine1.turnCrank();
+        gumball_machine1.insertCoin(1);
+        gumball_machine1.turnCrank();
+        gumball_machine1.insertCoin(10);
+        gumball_machine1.turnCrank();
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.turnCrank();
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.turnCrank();
         
-        GumballMachine gumballMachine = new GumballMachine(num_of_gumballs, "machine1");
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.turnCrank();
         
-
-        System.out.println("\n"+machine1 + "-- Scenario 1: Turn crank without any coin");
-        machine1.turnCrank();
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.insertCoin(5);
+        gumball_machine1.insertCoin(5);
+        gumball_machine1.insertCoin(5);
+        gumball_machine1.insertCoin(10);
+        gumball_machine1.turnCrank();
         
-        System.out.println("\n"+machine1 + "-- Scenario 2: Turn crank with only 1 quarter");
-        machine1.insertCoin(quarter);
-        machine1.turnCrank();
-
-        System.out.println("\n"+machine1 + "-- Scenario 3: Turn crank with additional quarters");
-        machine1.insertCoin(quarter);
-        machine1.insertCoin(quarter);
-        machine1.insertCoin(quarter);
-        machine1.turnCrank();
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.turnCrank();
         
-        System.out.println("\n"+machine1 + "-- Scenario 4: Turn crank with no gumballs");
-        machine1.insertCoin(quarter);
-        machine1.turnCrank();
-        machine1.insertCoin(quarter);
-        machine1.turnCrank();
-        machine1.insertCoin(quarter);
-        machine1.turnCrank();
+        gumball_machine1.insertCoin(25);
+        gumball_machine1.turnCrank();
+
+        /*System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);*/
         
-        Machine2 machine2 = new Machine2(2);
-
-        System.out.println("\n" +machine1 + "-- Scenario 1: Turn crank without any cent");
-        machine2.turnCrank();
-
-        System.out.println(machine1 + "-- Scenario 2: Turn crank with less than 50 cents");
-
-        machine2.insertCoin(quarter);
-        machine2.turnCrank();
-        machine2.insertCoin(quarter);
-        machine2.insertCoin(quarter);
-        machine2.turnCrank();
-        machine2.insertCoin(quarter);
-        machine2.insertCoin(quarter);
-        machine2.turnCrank();
-        System.out.println(machine2);
-        
-        Machine3 machine3 = new Machine3(1);
-
-        System.out.println(machine3);
-
-        machine3.insertCoin(nickle);
-        machine3.turnCrank();
-
-        System.out.println(machine3);
-        machine3.insertCoin(quarter);
-        machine3.turnCrank();
-        machine3.insertCoin(quarter);
-        machine3.insertCoin(quarter);
-        machine3.turnCrank();
-        machine3.insertCoin(quarter);
-        machine3.insertCoin(dime);
-        machine3.insertCoin(dime);
-        machine3.insertCoin(nickle);
-        machine3.turnCrank();
-        System.out.println(machine3);
-	}
+    }
 }
